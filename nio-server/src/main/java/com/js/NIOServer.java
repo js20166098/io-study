cbaf5741f260a64028192a3f5e074f2bee5dd7a5
姜爽 <jiangshuang@wejoydata.com>
2022-03-14 14:15:24 +0800

    NIO 学习

--- nio-server/src/main/java/com/js/NIOServer.java
+++ nio-server/src/main/java/com/js/NIOServer.java
@@ -70,7 +70,7 @@ public class NIOServer {
                     //设置成非阻塞
                     channel.configureBlocking(false);
                     //在这里可以给客户端发送信息哦
-                    channel.write(ByteBuffer.wrap("向客户端发送了一条信息".getBytes()));
+                    channel.write(ByteBuffer.wrap("Hello Client".getBytes()));
                     //在和客户端连接成功之后，为了可以接收到客户端的信息，需要给通道设置读的权限。
                     channel.register(this.selector, SelectionKey.OP_READ);
 
